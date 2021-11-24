7582b18bdc865fa7179805fd7ef370819daae648
Marco Julián Torre <marco.julian.torre@gmail.com>
2021-11-24 09:40:51 -0300

    implemented server code for sum unary call

--- server/server.go
+++ server/server.go
@@ -1,7 +1,36 @@
 package main
 
-import "log"
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/marcojulian/calculator/calculatorpb"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	calculatorpb.UnimplementedCalculatorServiceServer
+}
+
+func (*server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculatorpb.SumResponse, error) {
+	log.Printf("Sum function was invoked with %v", req)
+	return &calculatorpb.SumResponse{
+		Result: req.GetNum1() + req.GetNum2(),
+	}, nil
+}
 
 func main() {
 	log.Println("Hello I'm a server!")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
